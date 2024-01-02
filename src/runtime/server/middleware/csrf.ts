13999e64da0b993220ebf6a2f8b3a75f75e010ac
Mark Powell <mpowell@atomicsoftware.com>
2024-01-02 09:06:17 -0500

    Check request body for 'csrf-token' form input
    Allow csrf token check to use form input in case where there is a regular form action submit

--- src/runtime/server/middleware/csrf.ts
+++ src/runtime/server/middleware/csrf.ts
@@ -13,7 +13,8 @@ export default defineEventHandler(async (event) => {
   if (!methodsToProtect.includes(method)) { return }
 
   const secret = getCookie(event, csrfConfig.cookieKey!) ?? ''
-  const token = getHeader(event, 'csrf-token') ?? ''
+  const body = await readBody(event)
+  const token = getHeader(event, 'csrf-token') ?? body['csrf-token'] ?? '';
   // verify the incoming csrf token
   const url = event.node.req.url ?? ''
   const excluded = excludedUrls.some(el => Array.isArray(el)
